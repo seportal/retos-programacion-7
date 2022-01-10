c55b951e303b9103d98cc33de3c75809fbe5cce4
Selva Portal <selvaportal36@gmail.com>
2022-01-10 20:46:08 -0300

    finalizado

--- retos.test.js
+++ retos.test.js
@@ -7,33 +7,34 @@ const {
   forEach,
   map,
   filter,
-} = require('./retos');
+} = require("./retos");
 
-describe('mayuscula(nombre)', function () {
-  it('should return the same name with the first letter capitalized', function () {
-    expect(mayuscula('mario')).toBe('Mario');
-    expect(mayuscula('ana')).toBe('Ana');
+describe("mayuscula(nombre)", function () {
+  it("should return the same name with the first letter capitalized", function () {
+    expect(mayuscula("mario")).toBe("Mario");
+    expect(mayuscula("ana")).toBe("Ana");
   });
 });
 
-describe('invocarCallback(cb)', function () {
-  it('should invoke the callback that is passed in', function () {
+describe("invocarCallback(cb)", function () {
+  it("should invoke the callback that is passed in", function () {
     const cb = jest.fn();
     invocarCallback(cb);
     expect(cb).toHaveBeenCalled();
   });
 });
 
-describe('operacionMatematica(n1, n2, cb)', function () {
-  it('should return the callback function passing it the received arguments', function () {
-    const cb = jest.fn();
-    operacionMatematica(100, 20, cb);
+describe("operacionMatematica(n1, n2, cb)", function () {
+  it("should return the callback function passing it the received arguments", function () {
+    const cb = jest.fn((x, y) => x + y);
+    const res = operacionMatematica(100, 20, cb);
     expect(cb).toHaveBeenCalled();
+    expect(res).toBe(120);
   });
 });
 
-describe('sumarArray(cb)', function () {
-  it('should pass the sum of all array numbers to cb', function (done) {
+describe("sumarArray(cb)", function () {
+  it("should pass the sum of all array numbers to cb", function (done) {
     sumarArray([1, 2, 3, 4, 5], function (sum) {
       expect(sum).toBe(15);
       done();
@@ -41,8 +42,8 @@ describe('sumarArray(cb)', function () {
   });
 });
 
-describe('forEach(arr, cb)', function () {
-  it('should pass all array items one by one to cb', function () {
+describe("forEach(arr, cb)", function () {
+  it("should pass all array items one by one to cb", function () {
     const nums = [];
     forEach([1, 2, 3, 4, 5], function (num) {
       nums.push(num);
@@ -51,8 +52,8 @@ describe('forEach(arr, cb)', function () {
   });
 });
 
-describe('map(arr, cb)', function () {
-  it('should return an array of all the processed array elements', function () {
+describe("map(arr, cb)", function () {
+  it("should return an array of all the processed array elements", function () {
     const squares = map([1, 2, 3, 4, 5], function (num) {
       return num * num;
     });
@@ -60,18 +61,18 @@ describe('map(arr, cb)', function () {
   });
 });
 
-describe('filter(array)', function () {
+describe("filter(array)", function () {
   it('should return an array conteining the words that starts with "a"', function () {
     var array = [
-      'abajo',
-      'pera',
-      'escalera',
-      'alerta',
-      'indice',
-      'azteca',
-      'arbol',
-      'buzo',
+      "abajo",
+      "pera",
+      "escalera",
+      "alerta",
+      "indice",
+      "azteca",
+      "arbol",
+      "buzo",
     ];
-    expect(filter(array)).toEqual(['abajo', 'alerta', 'azteca', 'arbol']);
+    expect(filter(array)).toEqual(["abajo", "alerta", "azteca", "arbol"]);
   });
 });
